6a144ed742f46b9eb1b9068a41f476bb2b912f23
mitchelparish <mitchelparish@gmail.com>
2019-04-03 17:31:27 -0600

    file tree

---
+++ config/environment.rb
@@ -0,0 +1,9 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+
+require_relative '../lib/red_rocks/scraper'
+require_relative '../lib/red_rocks/concert'
+require_relative '../lib/red_rocks/cli'
+require_relative '../lib/red_rocks/version'
--- lib/red/rocks/version.rb
+++
@@ -1,5 +0,0 @@
-module Red
-  module Rocks
-    VERSION = "0.1.0"
-  end
-end
